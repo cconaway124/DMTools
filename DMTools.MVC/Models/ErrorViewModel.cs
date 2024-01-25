9c2b4ea78c552cad255825f55717ab6e12f3a929
Chase1242 <chaseconaway82@gmail.com>
2024-01-24 21:51:47 -0800

    Delete other MVC

--- DMTools.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DMTools.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
