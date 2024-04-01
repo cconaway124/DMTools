84ef345d605013ed5c8c6ed1dbeb0bbe6ad1d977
chase.conaway <chase.conaway@wsu.edu>
2024-04-01 16:35:56 -0700

    Idk man

--- DMTools/DMTools/Program.cs
+++ DMTools/DMTools/Program.cs
@@ -52,7 +52,7 @@ else
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
-//app.UseAntiforgery();
+app.UseAntiforgery();
 
 app.MapGet("/Account/Login", async (HttpContext httpContext, string redirectUri = "/") =>
 {
